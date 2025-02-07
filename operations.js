6934a3d8a0527a321617917c4aba2f71628c0461
KM Rejoan Tanjim <kmrejoantanjim@gmail.com>
2025-02-08 02:41:32 +0600

    practice basic operations

---
+++ operations.js
@@ -0,0 +1,35 @@
+// operation
+// 1. addition   => +
+// 2. subtraction  => -
+// 3. multiplication => *
+// 4. division  => /
+// 5. power of  => **
+// 6. modules => % (remember of division)
+
+//addition
+const num1 = 10;
+let num2;
+num2 = 10;
+
+let additionTotal = num1 + num2;
+
+console.log(additionTotal);
+
+//subtraction
+let subtractionTotal = num1 - num2;
+console.log(subtractionTotal);
+
+// multiplication
+let multiplicationTotal = num1 * num2;
+console.log(multiplicationTotal);
+
+// division
+let divisionTotal = num1 / num2;
+console.log(divisionTotal);
+
+// power of
+let powerOfTotal = num1 ** num2;
+console.log(powerOfTotal);
+
+let modulesTotal = num1 % num2;
+console.log(modulesTotal);
