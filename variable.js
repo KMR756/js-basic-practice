cd9f8a9ef0c74cb378521af9c5533f97c84f1c05
KM Rejoan Tanjim <kmrejoantanjim@gmail.com>
2025-02-08 01:53:14 +0600

    practice variable in js

---
+++ variable.js
@@ -0,0 +1,13 @@
+// const accountId = 1234; //const data cant change
+// let accountEmail = "eample@gmail.com"; //let can be change
+
+// console.log(accountId);
+// console.table([accountId, accountEmail]);
+
+const userId = 123;
+const userName = "rahat123";
+let firstName = "Rahat";
+let lastName = "Khan";
+let password = 123456;
+
+console.table([userId, userName, firstName, lastName, password]);
