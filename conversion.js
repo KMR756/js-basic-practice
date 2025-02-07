9bfcacaf7ab0cf82a902bf700324e4eafa15a5e1
KM Rejoan Tanjim <kmrejoantanjim@gmail.com>
2025-02-08 02:26:01 +0600

    practice different type of conversion

---
+++ conversion.js
@@ -0,0 +1,16 @@
+// let score = "33";
+// let valueInNumber = Number(score);
+
+// console.log(typeof score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let age = 25;
+// let valueInString = String(age);
+// console.log(typeof valueInString);
+// console.log(valueInString);
+
+let IsProgrammer = " "; //if have any data the output true otherwise false .
+let valueInBoolean = Boolean(IsProgrammer);
+console.log(typeof valueInBoolean);
+console.log(valueInBoolean);
